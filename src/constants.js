5fd572e2cb13257d14e5d3c91cd1c7f22e13916c
Shwetanshu Raj <shwetanshu@Shwetanshus-MacBook-Air.local>
2023-03-20 02:42:16 +0530

    added the url for the live django server

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 // constains all the constants
 
-export const BASE_URL = "http://127.0.0.1:8000/";
+export const BASE_URL = "http://18.183.206.230/";
 export const LOGIN_API_URL = BASE_URL + "api/login/";
 export const REGISTER_API_URL = BASE_URL + "api/register_user/";
 export const LOGOUT_API_URL = BASE_URL + "api/logout/";
